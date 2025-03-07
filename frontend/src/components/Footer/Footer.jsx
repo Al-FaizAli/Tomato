080c9ad6d70176f91dcd6255552ee16f46028461
Al Faiz Ali <alfaizali8410@gmail.com>
2025-03-08 03:14:34 +0530

    updated

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,42 +3,53 @@ import './Footer.css'
 import { assets } from '../../assets/assets'
 
 const Footer = () => {
-  return (
-    <div className='footer' id='footer'>
-        <div className="footer-content">
-            <div className="footer-content-left">
-                <img src={assets.logo} alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum in, beatae dolorem non optio cupiditate, quam sunt dicta dolores minima exercitationem ducimus totam aut asperiores inventore harum laudantium. Distinctio, libero.</p>
-                <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum in, beatae dolorem non optio cupiditate, quam sunt dicta dolores minima exercitationem ducimus totam aut asperiores inventore harum laudantium. Distinctio, libero.</p>
+                    <div className="footer-social-icons">
+                        <a href="https://www.facebook.com/profile.php?id=100028977910485" target="_blank" rel="noopener noreferrer">
+                            <img src={assets.facebook_icon} alt="Facebook" />
+                        </a>
+                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                            <img src={assets.twitter_icon} alt="Twitter" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/al-faiz-ali-88299223b/" target="_blank" rel="noopener noreferrer">
+                            <img src={assets.linkedin_icon} alt="LinkedIn" />
+                        </a>
+                    </div>
                 </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>
+                            <a href="tel:+919569715986">+91 95697 15986</a>
+                        </li>
+                        <li>
+                            <a href="mailto:alfaizali6780@gmail.com">alfaizali6780@gmail.com</a>
+                        </li>
+                    </ul>
+
+                </div>
+
             </div>
-            <div className="footer-content-center">
-                <h2>COMPANY</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </div>
-            <div className="footer-content-right">
-                <h2>GET IN TOUCH</h2>
-                <ul>
-                    <li>+94 765489545</li>
-                    <li>dulanjalisenarathna93@gmail.com</li>
-                </ul>
-            </div>
-           
+            <hr />
+            <p className="footer-copyright">
+                Copyright 2024 &copy; Dulanjali - All Right Reserved.
+            </p>
         </div>
-        <hr />
-        <p className="footer-copyright">
-            Copyright 2024 &copy; Dulanjali - All Right Reserved.
-        </p>
-    </div>
-  )
+    )
 }
 
 export default Footer
