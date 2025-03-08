3ab1ca8d8b9b15b6c5c2489263f314ca5b543473
Al Faiz Ali <alfaizali8410@gmail.com>
2025-03-09 01:15:13 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -55,8 +55,8 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: "*",
-    credentials: true
+    origin: "*"
+    // credentials: true
 }));
 // app.use(cors({
 //     origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL].filter(Boolean),
