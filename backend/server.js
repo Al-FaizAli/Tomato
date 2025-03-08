df0c4537ad9c862a47f4a597ee99a525394c4fd8
Al Faiz Ali <alfaizali8410@gmail.com>
2025-03-08 16:20:24 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || "*", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL ||process.env.FRONTEND_URL || "*", credentials: true }));
 
 // Database Connection
 connectDB();
