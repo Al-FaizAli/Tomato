af7c74a7977498031e9e6c1062d2747380dd3efd
Al Faiz Ali <alfaizali8410@gmail.com>
2025-03-09 04:03:07 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -54,14 +54,31 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-    origin: "*"
-    // credentials: true
-}));
+// app.use(cors({
+//     origin: "*"
+//     // credentials: true
+// }));
 // app.use(cors({
 //     origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL].filter(Boolean),
 //     credentials: true
 // }));
+
+const allowedOrigins = [
+    "https://tomato-admin-vert.vercel.app",
+    "https://tomato-al-faizs-projects.vercel.app",
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
+
 
 // Database Connection
 connectDB();
