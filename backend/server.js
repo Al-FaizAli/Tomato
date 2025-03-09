7f25c4e388f11e20fb83f199260dac6d62ff23d7
Al Faiz Ali <alfaizali8410@gmail.com>
2025-03-09 05:55:05 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,12 @@ const port = 5000
 
 // middleware
 app.use(express.json())
-app.use(cors({
-    origin: ["https://tomato-admin-vert.vercel.app"], // Allow only your frontend
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-    credentials: true // Allow cookies if needed
-}));
+const corsOptions = {
+    origin: /\.onrender\.com$/,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+app.use(cors(corsOptions));
+
 
 
 //db connection
