f336d93a31aa42bee8e63f0ae4a4ef668855c743
Al Faiz Ali <140162950+Al-FaizAli@users.noreply.github.com>
2025-03-10 03:48:52 +0530

    Update StoreContext.jsx

--- frontend/src/components/context/StoreContext.jsx
+++ frontend/src/components/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
     // const url = "http://localhost:5000";
-    const url = "https://foodcorner-1.onrender.com";
+    const url = "https://tomato-backend-f4y1.onrender.com";
     const [token, setToken] = useState("");
 
     const [food_list, setFoodList] = useState([]);
