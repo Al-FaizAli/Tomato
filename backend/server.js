648a61118216126489ce2c31f882a23867becd56
Al Faiz Ali <alfaizali8410@gmail.com>
2025-03-09 05:38:29 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,12 @@ const port = 5000
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: "*"
-    // origin: process.env.FRONTEND_URL,
-    // credentials: true
+    origin: ["https://tomato-admin-vert.vercel.app"], // Allow only your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    credentials: true // Allow cookies if needed
 }));
+
 
 //db connection
 connectDB();
